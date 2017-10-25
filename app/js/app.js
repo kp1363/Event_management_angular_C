4b17f9e1fd991f2f9ecc224c43fa721b53bbdda0
kp1363 <keyurp1363@gmail.com>
2017-10-25 13:39:53 -0700

    1st commit
    1st commit

---
+++ app/js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var eventsApp = angular.module('eventsApp', ['ngResource','ngSanitize', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+    $routeProvider.when('/newEvent',
+        {
+            templateUrl: 'templates/NewEvent.html',
+            controller: 'EditEventController'
+        });
+    $routeProvider.when('/events',
+         {
+             templateUrl: 'templates/EventList.html',
+             controller: 'EventListController'
+         });
+    $routeProvider.when('/event/:eventId',
+         {
+             templateUrl: 'templates/Eventdetails.html',
+             controller: 'EventController'
+         });
+    $routeProvider.otherwise({ redirectTo: '/events' });
+
+    $locationProvider.html5Mode(true);
+
+
+});
---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope, eventData, $log, $routeParams) {
+        $scope.sortorder = 'name',
+        $scope.snippet = '<span style="color:red">Hey there</span>',
+        eventData.getEvent($routeParams.eventId)
+            .$promise
+            .then(function (event) { $scope.event = event;})
+            .catch(function (response) { console.log(response);}
+);
+        
+        $scope.upVoteSession = function (session) {
+            session.upVoteCount++; 
+        }
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        }
+}
+
+);
